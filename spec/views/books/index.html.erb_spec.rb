9e856a4765fbedd50d7b06d0a13482240013ed0c
Hugo Silva <hugocardososilva@gmail.com>
2021-02-11 01:27:17 -0300

    test(book): fix view tests and ident layout

--- spec/views/books/index.html.erb_spec.rb
+++ spec/views/books/index.html.erb_spec.rb
@@ -1,25 +1,29 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "books/index", type: :view do
+RSpec.describe 'books/index', type: :view do
   before(:each) do
     assign(:books, [
-      Book.create!(
-        title: "Title",
-        description: "MyText",
-        active: false
-      ),
-      Book.create!(
-        title: "Title",
-        description: "MyText",
-        active: false
-      )
-    ])
+             Book.create!(
+               title: 'Title',
+               description: 'MyText',
+               active: false,
+               author: FactoryBot.create(:author)
+             ),
+             Book.create!(
+               title: 'Title',
+               description: 'MyText',
+               active: false,
+               author: FactoryBot.create(:author)
+             )
+           ])
   end
 
-  it "renders a list of books" do
+  it 'renders a list of books' do
     render
-    assert_select "tr>td", text: "Title".to_s, count: 2
-    assert_select "tr>td", text: "MyText".to_s, count: 2
-    assert_select "tr>td", text: false.to_s, count: 2
+    assert_select 'tr>td', text: 'Title'.to_s, count: 2
+    assert_select 'tr>td', text: 'MyText'.to_s, count: 2
+    assert_select 'tr>td', text: false.to_s, count: 2
   end
 end
--- spec/views/books/show.html.erb_spec.rb
+++ spec/views/books/show.html.erb_spec.rb
@@ -1,15 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "books/show", type: :view do
+RSpec.describe 'books/show', type: :view do
   before(:each) do
     @book = assign(:book, Book.create!(
-      title: "Title",
-      description: "MyText",
-      active: false
-    ))
+                            title: 'Title',
+                            description: 'MyText',
+                            active: false,
+                            author: FactoryBot.create(:author)
+                          ))
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
     expect(rendered).to match(/Title/)
     expect(rendered).to match(/MyText/)
