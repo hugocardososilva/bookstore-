71a361bbb34513afba81873a5b50c975f152effa
Hugo Silva <hugocardososilva@gmail.com>
2021-02-12 02:42:49 -0300

    fix(model): refactor mongoid document

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
-class Book < MongoidDocument
+class Book
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
   field :title, type: String
   field :description, type: String
   field :active, type: Mongoid::Boolean
