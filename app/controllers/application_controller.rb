2edc06665cba5e6da0ee00927b8ee1e1b04a81b0
Hugo Silva <hugocardososilva@gmail.com>
2021-02-14 15:38:10 -0300

    build(CI): add rescue from cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  include InheritedResources
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.new_user_session_path, alert: exception.message
+  end
 end
