dacaca10329057922d6897fe1303d467cd0edb02
Hugo Silva <hugocardososilva@gmail.com>
2021-02-17 03:57:35 -0300

    fix(book): fix some tests

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Image, type: :model do
   context 'associations ' do
     it { is_expected.to be_mongoid_document }
-    #FIXME: FIX ACTIVE RECORD CALL IN MONGOID
-    xit { is_expected.to belong_to(:book) }
+    it { is_expected.to belong_to(:book) }
   end
 end
