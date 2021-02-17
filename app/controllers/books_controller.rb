a0f89733e154f06e8ae35423a4c5eac56344123f
Hugo Silva <hugocardososilva@gmail.com>
2021-02-17 01:57:03 -0300

    feat(book): add pagination in controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,15 @@
 class BooksController < InheritedResources::Base
+  respond_to :json
 
+  def index
+   @current_page = params[:page]
+   @books = Book.page(@current_page).per(3)
+  end
 
   private
 
-    def book_params
-      params.require(:book).permit(:title, :description, :active, :author_id)
-    end
+  def book_params
+    params.require(:book).permit(:title, :description, :active, :author_id)
+  end
 
 end
